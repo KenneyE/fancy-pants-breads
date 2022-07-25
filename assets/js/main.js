ed743ae5788807bc4867489b7d9e45bea7222b6d
Eric Kenney <ekenney5@gmail.com>
2022-07-25 16:32:56 -0600

    Update site

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,9 +36,9 @@
 
 					// Images (in the format of 'url': 'alignment').
 						images: {
-							'images/bg01.jpg': 'center',
-							'images/bg02.jpg': 'center',
-							'images/bg03.jpg': 'center'
+							'images/bg-breads.jpg': 'center',
+							'images/bg-breads2.jpg': 'center',
+							'images/bg-breads3.jpg': 'center'
 						},
 
 					// Delay.
